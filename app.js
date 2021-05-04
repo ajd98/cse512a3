bf9b1422d0b857eb786fe1170d09de83d366aafe
Alex DeGrave <ajd98@pitt.edu>
2021-05-04 14:28:28 -0700

    fix double selections

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ data = data.filter(d => d.systolic_bp > 0 && d.poverty_index > 0);
 const incomeFilterWidth = 50;
 const ageFilterWidth = 5;
 
-const width = 400;
-const height = 400;
+const width = 600;
+const height = 600;
 const margin = {
   left: 40,
   right: 10,
@@ -26,7 +26,6 @@ const annotationLineColor = "#606060";
 const minBP = 60;
 const maxBP = 200
 const x = d3.scaleLinear()
-  //.domain([0, d3.max(data, d => d['systolic_bp'])])
   .domain([minBP, maxBP])
   .range([margin.left, width-margin.right])
   .nice();
@@ -34,7 +33,6 @@ const x = d3.scaleLinear()
 const minCholesterol = 50;
 const maxCholesterol = 400;
 const y = d3.scaleLinear()
-  //.domain([0, d3.max(data, d => d['serum_cholesterol'])])
   .domain([minCholesterol, maxCholesterol])
   .range([height - margin.bottom, margin.top])
   .nice();
@@ -45,8 +43,6 @@ data = data.filter(d => (minBP < d.systolic_bp)
                         && (minCholesterol < d.serum_cholesterol)
                         && (maxCholesterol > d.serum_cholesterol)
 );
-//temporary, for debugging
-//data = [data[0]];
 
 // create an svg
 const svg = d3.create('svg')
@@ -131,7 +127,8 @@ let participants = svg
     .attr('fill', d => backgroundDotColor)
     .attr('cx', d => x(d['systolic_bp']))
     .attr('cy', d => y(d['serum_cholesterol']))
-    .attr('r', d => 2);
+    .attr('r', d => 2)
+    .attr('class', 'dataElement');
 
 document.getElementById('mainChart').appendChild(svg.node());
 
@@ -148,6 +145,7 @@ let averages = {
 };
 let myAverageMark = null;
 let otherAverageMark = null;
+
 
 function onInputChange (e) {
   selectedAge = Number(ageInput.value);
@@ -173,10 +171,10 @@ function onInputChange (e) {
   averages.my.x = average(filteredData, 'systolic_bp');
   averages.my.y = average(filteredData, 'serum_cholesterol');
 
-  participants = participants
+  participants = svg.selectAll('.dataElement') // select elements of class dataElement
     .data(filteredData, d => d.index)
     .join(
-      enter => enter.remove().append('circle')
+      enter => enter
         .attr('fill', myDotColor)
         .attr('opacity', foregroundDotOpacity)
         .attr('cx', d => x(d.systolic_bp))
@@ -206,11 +204,39 @@ function onInputChange (e) {
   averages.other.x = average(filteredData, 'systolic_bp');
   averages.other.y = average(filteredData, 'serum_cholesterol');
 
-  participants = participants
+  participants = svg.selectAll('.dataElement') // select elements of class dataElement
     .data(filteredData, d => d.index)
     .join(
-      enter => enter.remove().append('circle')
+      enter => enter
         .attr('fill', otherDotColor)
+        .attr('opacity', foregroundDotOpacity)
+        .attr('cx', d => x(d.systolic_bp))
+        .attr('cy', d => y(d.serum_cholesterol))
+        .attr('r', d => 2),
+      update => update
+        .attr('fill', otherDotColor)
+        .attr('opacity', foregroundDotOpacity),
+      exit => exit, // dont need to do anything here
+    );
+
+  // filter the data for people in the intersection 
+  filteredData = data.filter(
+    d => (
+      (d.poverty_index > otherPovertyIndex - incomeFilterWidth) 
+      && (d.poverty_index < otherPovertyIndex + incomeFilterWidth)
+      && (d.poverty_index > selectedPovertyIndex - incomeFilterWidth)
+      && (d.poverty_index < selectedPovertyIndex + incomeFilterWidth)
+      && (d.age > selectedAge - ageFilterWidth)
+      && (d.age < selectedAge + ageFilterWidth)
+      && (d.sex == selectedSex)
+    )
+  );
+
+  participants = svg.selectAll('.dataElement') // select elements of class dataElement
+    .data(filteredData, d => d.index)
+    .join(
+      enter => enter
+        .attr('fill', intersectDotColor)
         .attr('opacity', foregroundDotOpacity)
         .attr('cx', d => x(d.systolic_bp))
         .attr('cy', d => y(d.serum_cholesterol))
@@ -232,48 +258,48 @@ function onInputChange (e) {
   } catch(err) {
     //pass
   }
-  myAverageMark = svg.append('g');
-  myAverageMark.append('line')
-    .attr('x1', x(averages.my.x))
-    .attr('y1', y(minCholesterol))
-    .attr('x2', x(averages.my.x))
-    .attr('y2', y(maxCholesterol))
-    .attr('stroke', myDotColor);
-  myAverageMark.append('line')
-    .attr('x1', x(minBP))
-    .attr('y1', y(averages.my.y))
-    .attr('x2', x(maxBP))
-    .attr('y2', y(averages.my.y))
-    .attr('stroke', myDotColor);
+  //myAverageMark = svg.append('g');
+  //myAverageMark.append('line')
+  //  .attr('x1', x(averages.my.x))
+  //  .attr('y1', y(minCholesterol))
+  //  .attr('x2', x(averages.my.x))
+  //  .attr('y2', y(maxCholesterol))
+  //  .attr('stroke', myDotColor);
+  //myAverageMark.append('line')
+  //  .attr('x1', x(minBP))
+  //  .attr('y1', y(averages.my.y))
+  //  .attr('x2', x(maxBP))
+  //  .attr('y2', y(averages.my.y))
+  //  .attr('stroke', myDotColor);
 
-  otherAverageMark = svg.append('g');
-  otherAverageMark.append('line')
-    .attr('x1', x(averages.other.x))
-    .attr('y1', y(minCholesterol))
-    .attr('x2', x(averages.other.x))
-    .attr('y2', y(maxCholesterol))
-    .attr('stroke', otherDotColor);
-  otherAverageMark.append('line')
-    .attr('x1', x(minBP))
-    .attr('y1', y(averages.other.y))
-    .attr('x2', x(maxBP))
-    .attr('y2', y(averages.other.y))
-    .attr('stroke', otherDotColor);
+  //otherAverageMark = svg.append('g');
+  //otherAverageMark.append('line')
+  //  .attr('x1', x(averages.other.x))
+  //  .attr('y1', y(minCholesterol))
+  //  .attr('x2', x(averages.other.x))
+  //  .attr('y2', y(maxCholesterol))
+  //  .attr('stroke', otherDotColor);
+  //otherAverageMark.append('line')
+  //  .attr('x1', x(minBP))
+  //  .attr('y1', y(averages.other.y))
+  //  .attr('x2', x(maxBP))
+  //  .attr('y2', y(averages.other.y))
+  //  .attr('stroke', otherDotColor);
 
-  //myAverageMark = svg.append('circle')
-  //  .attr('fill', myDotColor)
-  //  .attr('stroke', 'black')
-  //  .attr('opacity', 1)
-  //  .attr('cx', x(averages.my.x))
-  //  .attr('cy', y(averages.my.y))
-  //  .attr('r', 10);
-  //otherAverageMark = svg.append('circle')
-  //  .attr('fill', otherDotColor)
-  //  .attr('stroke', 'black')
-  //  .attr('opacity', 1)
-  //  .attr('cx', x(averages.other.x))
-  //  .attr('cy', y(averages.other.y))
-  //  .attr('r', 10);
+  myAverageMark = svg.append('circle')
+    .attr('fill', myDotColor)
+    .attr('stroke', 'black')
+    .attr('opacity', 1)
+    .attr('cx', x(averages.my.x))
+    .attr('cy', y(averages.my.y))
+    .attr('r', 5);
+  otherAverageMark = svg.append('circle')
+    .attr('fill', otherDotColor)
+    .attr('stroke', 'black')
+    .attr('opacity', 1)
+    .attr('cx', x(averages.other.x))
+    .attr('cy', y(averages.other.y))
+    .attr('r', 5);
   return Object.assign(svg.node(), {onInputChange});
 }
 
